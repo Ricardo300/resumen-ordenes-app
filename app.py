931883b474108b2630d6268f4f951941ce0522d7
Ricardo300 <64941166+Ricardo300@users.noreply.github.com>
2026-02-28 14:43:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ archivo = st.file_uploader("Sube el archivo Excel", type=["xlsx"])
 
 if archivo is not None:
     df = pd.read_excel(archivo)
-    
+
+    # Resumen por técnico
     resumen = df["Técnico"].value_counts().reset_index()
     resumen.columns = ["Técnico", "Órdenes"]
 
     st.subheader("Resultado:")
-
     st.dataframe(resumen)
+
+    # Resumen por contrata
     st.subheader("Resumen por Contrata")
-
-resumen_contrata = df["Contrata"].value_counts().reset_index()
-resumen_contrata.columns = ["Contrata", "Órdenes"]
-
-st.dataframe(resumen_contrata)
+    resumen_contrata = df["Contrata"].value_counts().reset_index()
+    resumen_contrata.columns = ["Contrata", "Órdenes"]
+    st.dataframe(resumen_contrata)
